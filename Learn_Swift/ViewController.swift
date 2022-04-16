a083624428f45ee4f808cf3cc2b8939ba3561832
minaje0917 <k050918@naver.com>
2022-04-16 14:58:21 +0900

    alert

--- Learn_Swift/ViewController.swift
+++ Learn_Swift/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    let realm = try! Realm()
+    //let tasks = TodoList
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "TodoList"
     }
-
+    
+    @IBAction func tapAdd(_sender: UIBarButtonItem){
+        let alert = UIAlertController(title: "TodoList", message: "Please enter your to-do", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "enter"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { action in
+            
+        }))
+        
+        self.present(alert, animated: true)
+    }
 
 }
 
+
+class TodoList: Object{
+    @Persisted var task:String = ""
+}
