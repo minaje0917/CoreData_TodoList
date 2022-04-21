963f2383c6ae3a1bd6dd4be2ec80b31524af19bf
minaje0917 <k050918@naver.com>
2022-04-21 23:06:23 +0900

    re

--- Learn_Swift/ViewController.swift
+++ Learn_Swift/ViewController.swift
@@ -8,27 +8,54 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     let field = UITextField()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var models = [TodoList]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.todo
+        return cell
+    }
     
     @IBAction func tapAdd(_sender: UIBarButtonItem){
         let alert = UIAlertController(title: nil, message: "Please enter your todo", preferredStyle: .alert)
         
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
         
         alert.addTextField { (textField) in
             self.field.placeholder = "enter"
             
         }
         
-        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { [self] action in
+        alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else{
+                return
+            }
+            
+            self?.createItem(todo: text)
             
             //self.tasks.todo = alert.textFields?[0].text ?? .init()
             //print("tasks: \(self.tasks.todo)")
@@ -42,7 +69,10 @@ class ViewController: UIViewController {
     
     func getAllItem() {
         do {
-            let items = try context.fetch(TodoList.fetchRequest())
+            models = try context.fetch(TodoList.fetchRequest())
+            DispatchQueue.main.sync {
+                self.tableView.reloadData()
+            }
         }
         catch{
             // error
@@ -50,16 +80,43 @@ class ViewController: UIViewController {
         
     }
     
-    func createItem(name: String) {
-        let newitem = TodoList(context: <#T##NSManagedObjectContext#>)
+    func createItem(todo: String) {
+        let newitem = TodoList(context: context)
+        newitem.todo = todo
+        newitem.date = Date()
+        
+        
+        do{
+            try context.save()
+            getAllItem()
+        }
+        catch{
+            
+        }
     }
     
     func deleteItem(item: TodoList) {
         
+        context.delete(item)
+        
+        do{
+            try context.save()
+        }
+        catch{
+            
+        }
+        
     }
     
-    func updateItem(item: TodoList) {
+    func updateItem(item: TodoList, newtodo: String) {
+        item.todo = newtodo
         
+        do{
+            try context.save()
+        }
+        catch{
+            
+        }
     }
 
 }
